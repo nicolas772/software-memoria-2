e8a75762c54cb1d103f66e8fc3127872d5fdd01c
Nicolas Araya Urrutia <nicolas.arayau@sansano.usm.cl>
2023-04-13 18:54:46 -0400

    Hola mundo frontend

--- frontend-memoria/src/App.jsx
+++ frontend-memoria/src/App.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-  //...
+  return(
+    <div>
+      <h1>
+        Hola Mundo
+      </h1>
+    </div>
+  )
 }
 
 export default App;
