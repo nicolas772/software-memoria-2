e012f71631e338a5cfced9bb57640ee78559381f
Nicolas Araya Urrutia <nicolas.arayau@sansano.usm.cl>
2023-07-09 20:22:15 -0400

    se crea componente navbar

--- frontend-memoria/src/App.jsx
+++ frontend-memoria/src/App.jsx
@@ -20,94 +20,12 @@ import IterationUser from "./components/user/IterationUser";
 import TaskUser from "./components/user/TaskUser";
 import FormCSUQ from "./components/user/FormCSUQ";
 import FormSentimentAnalisis from "./components/user/FormSentimentAnalisis";
-
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  const [showTesterBoard, setShowTesterBoard] = useState(false);
-  const [showUserBoard, setShowUserBoard] = useState(false);
-  const [currentUser, setCurrentUser] = useState(undefined);
-
-  useEffect(() => {
-    const user = AuthService.getCurrentUser();
-
-    if (user) {
-      setCurrentUser(user);
-      setShowUserBoard(user.roles.includes("ROLE_USER"));
-      setShowTesterBoard(user.roles.includes("ROLE_TESTER"));
-    }
-  }, []);
-
-  const logOut = () => {
-    AuthService.logout();
-  };
-
   return (
     <div>
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <Link to={"/home"} className="navbar-brand">
-          Proyecto Memoria
-        </Link>
-        <div className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <Link to={"/home"} className="nav-link">
-              Inicio
-            </Link>
-          </li>
-
-          {showTesterBoard && (
-            <>
-              <li className="nav-item">
-                <Link to={"/tester"} className="nav-link">
-                  Tester Board
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to={"/studies"} className="nav-link">
-                  Estudios
-                </Link>
-              </li>
-            </>
-          )}
-
-          {showUserBoard && (
-            <li className="nav-item">
-              <Link to={"/user"} className="nav-link">
-                User Board
-              </Link>
-            </li>
-          )}
-        </div>
-
-        {currentUser ? (
-          <div className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link to={"/profile"} className="nav-link">
-                @{currentUser.username}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <a href="/login" className="nav-link" onClick={logOut}>
-                Cerrar Sesión
-              </a>
-            </li>
-          </div>
-        ) : (
-          <div className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link to={"/login"} className="nav-link">
-                Iniciar Sesión
-              </Link>
-            </li>
-
-            <li className="nav-item">
-              <Link to={"/register"} className="nav-link">
-                Registrarse
-              </Link>
-            </li>
-          </div>
-        )}
-      </nav>
-
+      <Navbar></Navbar>
       <div className="container mt-3">
         <Routes>
           <Route path="/home" element={<Home />} />
---
+++ frontend-memoria/src/components/Navbar.jsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import AuthService from "../services/auth.service";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [showTesterBoard, setShowTesterBoard] = useState(false);
+  const [showUserBoard, setShowUserBoard] = useState(false);
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  useEffect(() => {
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      setCurrentUser(user);
+      setShowUserBoard(user.roles.includes("ROLE_USER"));
+      setShowTesterBoard(user.roles.includes("ROLE_TESTER"));
+    }
+  }, []);
+
+  const logOut = () => {
+    AuthService.logout();
+  };
+
+  return (
+    <>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <Link to={"/home"} className="navbar-brand">
+          Proyecto Memoria
+        </Link>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/home"} className="nav-link">
+              Inicio
+            </Link>
+          </li>
+
+          {showTesterBoard && (
+            <>
+              <li className="nav-item">
+                <Link to={"/tester"} className="nav-link">
+                  Tester Board
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/studies"} className="nav-link">
+                  Estudios
+                </Link>
+              </li>
+            </>
+          )}
+
+          {showUserBoard && (
+            <li className="nav-item">
+              <Link to={"/user"} className="nav-link">
+                User Board
+              </Link>
+            </li>
+          )}
+        </div>
+
+        {currentUser ? (
+          <div className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to={"/profile"} className="nav-link">
+                @{currentUser.username}
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a href="/login" className="nav-link" onClick={logOut}>
+                Cerrar Sesión
+              </a>
+            </li>
+          </div>
+        ) : (
+          <div className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to={"/login"} className="nav-link">
+                Iniciar Sesión
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link to={"/register"} className="nav-link">
+                Registrarse
+              </Link>
+            </li>
+          </div>
+        )}
+      </nav>
+    </>
+  )
+
+}
+
+export default Navbar;
