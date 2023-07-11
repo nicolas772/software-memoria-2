3c38099dfebd7fbf8d5b868b63a6df5d588db5d5
Nicolas Araya Urrutia <nicolas.arayau@sansano.usm.cl>
2023-07-11 00:21:41 -0400

    se modifican estilos para login

--- frontend-memoria/src/components/Login.jsx
+++ frontend-memoria/src/components/Login.jsx
@@ -52,9 +52,9 @@ const Login = () => {
     if (checkBtn.current.context._errors.length === 0) {
       AuthService.login(username, password).then(
         (response) => {
-          if (response.roles.includes("ROLE_USER")){
+          if (response.roles.includes("ROLE_USER")) {
             navigate("/homeUser");
-          }else {
+          } else {
             navigate("/homeTester");
           }
         },
@@ -76,60 +76,71 @@ const Login = () => {
   };
 
   return (
-    <div className="col-md-12">
-      <div className="card card-container">
-        <Form onSubmit={handleLogin} ref={form}>
-          <div className="form-group">
-            <label htmlFor="username">Usuario</label>
-            <Input
-              type="text"
-              className="form-control"
-              name="username"
-              value={username}
-              onChange={onChangeUsername}
-              validations={[required]}
-            />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="password">Contraseña</label>
-            <Input
-              type="password"
-              className="form-control"
-              name="password"
-              value={password}
-              onChange={onChangePassword}
-              validations={[required]}
-            />
-          </div>
-
-          <div className="form-group">
-            <button className="btn btn-primary btn-block" disabled={loading}>
-              {loading && (
-                <span className="spinner-border spinner-border-sm"></span>
-              )}
-              <span>Iniciar Sesión</span>
-            </button>
-          </div>
-
-          <div className="form-group">
-            <button className="btn btn-secondary btn-block" disabled={loading} onClick={handleRegister}>
-              {loading && (
-                <span className="spinner-border spinner-border-sm"></span>
-              )}
-              <span>Registrarse</span>
-            </button>
-          </div>
-
-          {message && (
-            <div className="form-group">
-              <div className="alert alert-danger" role="alert">
-                {message}
-              </div>
+    <div className="login-container">
+      <div className="col-md-6">
+        <div className="left-content">
+            <h1>Feel UX</h1>
+            <h6>Feel UX te ayudará a realizar de manera sencilla tus pruebas de usabilidad, utilizando Inteligencia 
+              Artificial para el análisis de sentimiento de tus usuarios.
+            </h6>
+        </div>
+      </div>
+      <div className="col-md-6">
+      
+        <div className="card card-container">
+          <Form onSubmit={handleLogin} ref={form}>
+            <div className="form-groups">
+              <label htmlFor="username">Usuario</label>
+              <Input
+                type="text"
+                className="form-control"
+                name="username"
+                value={username}
+                onChange={onChangeUsername}
+                validations={[required]}
+              />
             </div>
-          )}
-          <CheckButton style={{ display: "none" }} ref={checkBtn} />
-        </Form>
+
+            <div className="form-group">
+              <label htmlFor="password">Contraseña</label>
+              <Input
+                type="password"
+                className="form-control"
+                name="password"
+                value={password}
+                onChange={onChangePassword}
+                validations={[required]}
+              />
+            </div>
+
+            <div className="form-group">
+              <button className="btn btn-primary btn-block" disabled={loading}>
+                {loading && (
+                  <span className="spinner-border spinner-border-sm"></span>
+                )}
+                <span>Iniciar Sesión</span>
+              </button>
+            </div>
+
+            <div className="form-group">
+              <button className="btn btn-secondary btn-block" disabled={loading} onClick={handleRegister}>
+                {loading && (
+                  <span className="spinner-border spinner-border-sm"></span>
+                )}
+                <span>Registrarse</span>
+              </button>
+            </div>
+
+            {message && (
+              <div className="form-group">
+                <div className="alert alert-danger" role="alert">
+                  {message}
+                </div>
+              </div>
+            )}
+            <CheckButton style={{ display: "none" }} ref={checkBtn} />
+          </Form>
+        </div>
       </div>
     </div>
   )
