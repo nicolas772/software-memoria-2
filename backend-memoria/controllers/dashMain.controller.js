5b6034b5338e68f6099816991f12cffa25e8110c
nicolas772 <nicolas.arayau@sansano.usm.cl>
2023-10-22 17:57:08 -0300

    query para porcentaje de estudios completados

--- backend-memoria/controllers/dashMain.controller.js
+++ backend-memoria/controllers/dashMain.controller.js
@@ -25,25 +25,37 @@ exports.testerBoard = async (req, res) => {
       iterations.push(...study.iterations);
       return iterations;
     }, []);
-
+    //CARD 1: Iteraciones Activas
     const activeIterationsCount = allIterations.filter(iteration => iteration.state === "Activa").length;
     
-
+    //CARD 2: Usuarios Participantes
     const usersQtyCompleteSum = await Iteration.sum('users_qty_complete', {
       where: {
         id: allIterations.map(iteration => iteration.id)
       }
     });
 
+    //CARD 3: % Iteraciones Completadas
     const totalIterationsCount = allIterations.length;
-    const finishIterationsCount = allIterations.filter(iteration => iteration.state === "Finalizada").length;
-    const percentageFinishIterations = (finishIterationsCount / totalIterationsCount) * 100;
+    const finishedIterationsCount = allIterations.filter(iteration => iteration.state === "Finalizada").length;
+    const percentageFinishedIterations = (finishedIterationsCount / totalIterationsCount) * 100;
+
+    //CARD 4: % Estudios Completados
+
+    const allStudies = user.studies;
+    const totalStudiesCount = allStudies.length;
+    const finishedStudies = allStudies.filter(study => {
+      return study.iterations.every(iteration => iteration.state === "Finalizada");
+    });
+    const finishedStudiesCount = finishedStudies.length;
+
+    const percentageFinishedStudies = (finishedStudiesCount / totalStudiesCount) * 100;
 
     const responseData = {
       iteraciones_activas: activeIterationsCount,
       usuarios_participantes: usersQtyCompleteSum, // Obtén la cantidad de usuarios activos según tus necesidades.
-      porc_iteraciones_completadas: percentageFinishIterations.toFixed(1) + "%", // Calcula esto según tus necesidades.
-      porc_estudios_completados: "94%" // Calcula esto según tus necesidades.
+      porc_iteraciones_completadas: percentageFinishedIterations.toFixed(1) + "%", // Calcula esto según tus necesidades.
+      porc_estudios_completados: percentageFinishedStudies.toFixed(1) + "%" // Calcula esto según tus necesidades.
     };
 
     res.status(200).json(responseData);
