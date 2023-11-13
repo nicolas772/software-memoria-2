b688d7688cea421474328dda5490e3b773e4030f
nicolas772 <nicolas.arayau@sansano.usm.cl>
2023-11-13 15:36:59 -0300

    nuevos componentes para metric cards de KPI

--- frontend-memoria/src/components/tester/Dashboards/DashboardGeneralStudy.jsx
+++ frontend-memoria/src/components/tester/Dashboards/DashboardGeneralStudy.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import MetricCardList from '../../charts/MetricCardList';
+import MetricCardCategoryBar from '../../charts/MetricCardCategoryBar';
 
 export default function DashboardGeneralStudy(props) {
     const { content } = props;
     return (
         <div>
-          Dashboard General Study
+          <MetricCardCategoryBar />
         </div>
       );
 }
