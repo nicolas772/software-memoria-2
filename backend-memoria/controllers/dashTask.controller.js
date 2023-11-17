b139e7a9e6cc625e3eb2d739f0894f4f1cb5bfdd
nicolas772 <nicolas.arayau@sansano.usm.cl>
2023-11-17 17:40:20 -0300

    query para cards en dashboard general task

--- backend-memoria/controllers/dashTask.controller.js
+++ backend-memoria/controllers/dashTask.controller.js
@@ -8,12 +8,62 @@ const { Op } = require('sequelize'); // Necesitas importar Op desde sequelize
 exports.cards = async (req, res) => {
    const idTask = req.query.idTask;
    try {
-      const responseData = {
-         porcentaje_exito: "95.7%",
-         tiempo_promedio: "2m 34s",
-         tiempo_optimo: "3m 0s",
-         diferencia: "0m 26s"// Calcula esto según tus necesidades.
-      };
+      const task = await Task.findOne({
+         where: {
+            id: idTask
+         }
+      })
+      const allInfoTasks = await InfoTask.findAll({
+         where: {
+            taskId: idTask,
+         }
+      })
+
+      if (!allInfoTasks || !task) {
+         return res.status(404).json({ error: "Tarea No Encontrada." });
+      }
+      const info_task_qty = allInfoTasks.length
+
+
+      //CARD 3: Tiempo Optimo
+
+      const minutesOptimal = task.minutes_optimal
+      const secondsOptimal = task.seconds_optimal
+      const optimalTime = minutesOptimal + "m " + secondsOptimal + "s"
+      let responseData
+
+      if (info_task_qty > 0) {
+         //CARD 1: Porcentaje de éxito de tarea
+         const completedTasks = allInfoTasks.filter(task => task.complete === true).length;
+         const avgSuccessNoFormat = info_task_qty / completedTasks
+         const avgSuccess = (avgSuccessNoFormat * 100).toFixed(1) + "%";
+
+         //CARD 2: Tiempo promedio de tarea
+
+         const totalDuration = allInfoTasks.reduce((total, task) => total + task.duration, 0);
+         const avgDuration = totalDuration / info_task_qty;
+         const roundedAverageDuration = Math.round(avgDuration / 1000) * 1000;
+
+         //CARD 4: Diferencia Tiempo
+
+         const optTimeMili = minutesSecondsToMilliseconds(minutesOptimal, secondsOptimal);
+         const diference = Math.abs(roundedAverageDuration - optTimeMili)
+         responseData = {
+            porcentaje_exito: avgSuccess,
+            tiempo_promedio: formatTime(roundedAverageDuration),
+            tiempo_optimo: optimalTime,
+            diferencia: formatTime(diference)
+         };
+      } else {
+         responseData = {
+            porcentaje_exito: "0%",
+            tiempo_promedio: formatTime(0),
+            tiempo_optimo: optimalTime,
+            diferencia: formatTime(0)
+         };
+      }
+
+
 
       res.status(200).json(responseData);
    } catch (error) {
@@ -87,3 +137,16 @@ exports.barChart = async (req, res) => {
       res.status(500).json({ error: "Ha ocurrido un error al obtener los datos" });
    }
 };
+
+// Función para convertir milisegundos a "m minutos s segundos"
+function formatTime(milliseconds) {
+   const totalSeconds = Math.floor(milliseconds / 1000);
+   const minutes = Math.floor(totalSeconds / 60);
+   const remainingSeconds = totalSeconds % 60;
+   return `${minutes}m ${remainingSeconds}s`;
+}
+
+function minutesSecondsToMilliseconds(minutes, seconds) {
+   const totalSeconds = minutes * 60 + seconds;
+   return totalSeconds * 1000; // Convertir segundos a milisegundos
+}
