3d73dbd9bea3d90dc366fd61e805e7bd493d5f30
nicolas772 <nicolas.arayau@sansano.usm.cl>
2023-11-27 21:37:59 -0300

    modificacion de funcion create en open answers controller para agregar keywords a modelo correspondiente

--- backend-memoria/controllers/openanswer.controller.js
+++ backend-memoria/controllers/openanswer.controller.js
@@ -16,6 +16,113 @@ async function analizarSentimiento(texto) {
 exports.create = async (req, res) => {
   try {
     const opinion1 = req.body.opinion1;
+    const analisis = await analizarSentimiento(opinion1);
+    const keywords =
+      keyword_extractor.extract(opinion1, {
+        language: "spanish",
+        remove_digits: true,
+        return_changed_case: true,
+        remove_duplicates: false
+
+      });
+    const keywords_no_neutral = []
+    for (const keyword of keywords) {
+      const analisis_key = await analizarSentimiento(keyword);
+      if (analisis_key.vote != "neutral") {
+        keywords_no_neutral.push(keyword)
+        await Keyword.create({
+          userId: req.body.idUser,
+          iterationId: req.body.idIteration,
+          keyword: keyword,
+        });
+      }
+    }
+
+    await GeneralSentiment.create({
+      userId: req.body.idUser,
+      iterationId: req.body.idIteration,
+      answer: opinion1,
+      comparative: analisis.comparative,
+      numhits: analisis.numHits,
+      numwords: analisis.numWords,
+      score: analisis.score,
+      vote: analisis.vote
+    });
+
+    const iterationState = await IterationState.findOne({
+      where: { iterationId: req.body.idIteration, userId: req.body.idUser }
+    });
+    iterationState.inTask = false
+    iterationState.inCSUQ = false
+    iterationState.inQuestion = false
+    await iterationState.save()
+    //lo que sigue, es para disminuir en 1 la cantidad de usuarios activos completando la iteracion
+    //y aumentar en 1 la cantidad de usuarios que completaron la iteracion
+    const iteration = await Iteration.findByPk(req.body.idIteration)
+    iteration.users_qty -= 1
+    iteration.users_qty_complete += 1
+    await iteration.save()
+    const responseData = {
+      analisis_sentimiento: analisis,
+      keywords: keywords_no_neutral
+    };
+    res.status(200).json(responseData);
+  } catch (error) {
+    res.status(500).send('Error en el análisis de sentimiento'); // Enviar un mensaje de error en caso de excepción
+  }
+};
+
+exports.prueba = async (req, res) => {
+  try {
+    const opinion = req.body.opinion;
+    const analisis = await analizarSentimiento(opinion);
+    const keywords =
+      keyword_extractor.extract(opinion, {
+        language: "spanish",
+        remove_digits: true,
+        return_changed_case: true,
+        remove_duplicates: false
+
+      });
+    const keywords_no_neutral = []
+    for (const keyword of keywords) {
+      const analisis_key = await analizarSentimiento(keyword);
+      if (analisis_key.vote != "neutral") {
+        keywords_no_neutral.push(keyword)
+        await Keyword.create({
+          userId: 1,
+          iterationId: 1,
+          keyword: keyword,
+        });
+      }
+    }
+
+    await GeneralSentiment.create({
+      userId: 1,
+      iterationId: 1,
+      answer: opinion,
+      comparative: analisis.comparative,
+      numhits: analisis.numHits,
+      numwords: analisis.numWords,
+      score: analisis.score,
+      vote: analisis.vote
+    });
+
+    const responseData = {
+      analisis_sentimiento: analisis,
+      keywords: keywords_no_neutral
+    };
+
+    res.status(200).json(responseData);
+  } catch (error) {
+    res.status(500).send('Error en el análisis de sentimiento'); // Enviar un mensaje de error en caso de excepción
+  }
+};
+
+/*
+exports.create = async (req, res) => {
+  try {
+    const opinion1 = req.body.opinion1;
     const opinion2 = req.body.opinion2;
     const prefieroNoOpinar1 = req.body.prefieroNoOpinar1;
     const prefieroNoOpinar2 = req.body.prefieroNoOpinar2;
@@ -93,55 +200,4 @@ exports.create = async (req, res) => {
     res.status(500).send('Error en el análisis de sentimiento'); // Enviar un mensaje de error en caso de excepción
   }
 };
-
-exports.prueba = async (req, res) => {
-  try {
-    const opinion = req.body.opinion;
-    const analisis = await analizarSentimiento(opinion);
-    const keywords =
-      keyword_extractor.extract(opinion, {
-        language: "spanish",
-        remove_digits: true,
-        return_changed_case: true,
-        remove_duplicates: false
-
-      });
-    const keywords_no_neutral = []
-    for (const keyword of keywords) {
-      const analisis_key = await analizarSentimiento(keyword);
-      if (analisis_key.vote != "neutral") {
-        keywords_no_neutral.push(keyword)
-        await Keyword.create({
-          userId: 1,
-          iterationId: 1,
-          keyword: keyword,
-        });
-      }
-    }
-
-    await GeneralSentiment.create({
-      userId: 1,
-      iterationId: 1,
-      answer: opinion,
-      comparative: analisis.comparative,
-      numhits: analisis.numHits,
-      numwords: analisis.numWords,
-      score: analisis.score,
-      vote: analisis.vote
-    });
-
-
-
-
-
-
-    const responseData = {
-      analisis_sentimiento: analisis,
-      keywords: keywords_no_neutral
-    };
-
-    res.status(200).json(responseData);
-  } catch (error) {
-    res.status(500).send('Error en el análisis de sentimiento'); // Enviar un mensaje de error en caso de excepción
-  }
-};
+*/
