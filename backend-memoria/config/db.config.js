739c82d2d3ddad5c0f43e0d5b32f42312c4ab673
nicolas772 <nicolas.arayau@sansano.usm.cl>
2023-12-03 13:19:07 -0300

    conexion a elephantsql online

--- backend-memoria/config/db.config.js
+++ backend-memoria/config/db.config.js
@@ -1,13 +1,31 @@
+//Local
+/*module.exports = {
+  HOST: "localhost",
+  USER: "postgres",
+  PASSWORD: "posgres",
+  DB: "software-memoria-2",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};*/
+
+//Online
 module.exports = {
-    HOST: "localhost",
-    USER: "postgres",
-    PASSWORD: "posgres",
-    DB: "software-memoria-2",
-    dialect: "postgres",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
+  HOST: "isabelle.db.elephantsql.com",
+  USER: "gzwdxdbe",
+  PASSWORD: "CQcUdMCE41Cxwk_5fVotueMb9NMi6YZQ",
+  DB: "gzwdxdbe",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+"postgres://gzwdxdbe:CQcUdMCE41Cxwk_5fVotueMb9NMi6YZQ@isabelle.db.elephantsql.com/gzwdxdbe"
